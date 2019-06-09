3dfa9bd0d3408d99a3cc9b3e57fa608f913f6bf6
JakobCh <christoffersson.jakob@gmail.com>
2019-06-09 20:58:49 +0200

    Update customscripts.lua

--- customscripts.lua
+++ customscripts.lua
@@ -1,7 +1,13 @@
 require("custom.customSpells")
 require("custom.espParser")
-require("custom.espParserTest")
+--require("custom.espParserTest")
+require("custom.doorLinks")
 --require("custom.jcMarketplace")
 require("custom.jsonCellLoader")
 require("custom.loadtxt2esp")
 require("custom.bookWriting")
+
+
+decorateHelp = require("custom.decorateHelp")
+kanaFurniture = require("custom.kanaFurniture")
+kanaHousing = require("custom.kanaHousing")
