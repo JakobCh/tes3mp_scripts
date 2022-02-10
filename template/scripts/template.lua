11e8d1cfe2f40cf8f147889679ecb49bc497c6cf
JakobCh <christoffersson.jakob@gmail.com>
2022-02-10 10:40:02 +0100

    Create template.lua

---
+++ template/scripts/template.lua
@@ -0,0 +1,21 @@
+--[[
+
+Name: template
+Version: 0.1
+Tes3mp Version: 0.8
+Author: JakobCh
+Update date: 2022-02-10
+
+Description:
+    None
+
+Install:
+	Put this file in server/scripts/custom/
+	Put [ require("custom.template") ] in server/scripts/customScripts.lua
+
+Commands:
+    None
+
+]]
+
+-- This is just a template file for my other scripts
